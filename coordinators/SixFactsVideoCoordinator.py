e2c6e9b17fcaf1980e43695faf11e6608969643c
Truong Hong Loc <hongloc2206@gmail.com>
2024-06-12 17:32:01 +0700

    update

--- coordinators/SixFactsVideoCoordinator.py
+++ coordinators/SixFactsVideoCoordinator.py
@@ -48,11 +48,11 @@ class SixFactsVideoCoordinator:
             script = re.sub(r'[^\w\s.?!]', '', f['content'])
             video_maker.tts.synthesize(script, path)
             print("=> Wrote TTS to " + path)
-            tts_clip = AudioFileClip(path)
+            # tts_clip = AudioFileClip(path)
             # .set_fps(44100).set_duration(9.8).set_start(i*9.8).set_end((i+1)*9.8)
             image = image.set_audio(tts_clip)
 
-            image = zoom_in_effect(image.set_duration(9.8)\
+            image = zoom_in_effect(image.set_duration(9.8).set_audio(tts_clip)\
                         .set_start(i*9.8)\
                         .set_pos(("center", "center")))
             
